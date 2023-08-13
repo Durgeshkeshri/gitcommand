6254696092c2b13822ac8c363e146295df01f4b1
Durgesh <durgeshkeshri7@gmail.com>
2023-08-13 22:28:11 +0530

    add

--- sum.java
+++ sum.java
@@ -1,6 +1,8 @@
-public class sum{
-    public static void main(String[] args)
-    {
+public class sum {
+    public static void main(String[] args) {
         System.out.println("hello world");
+        for (int i = 0; i < 5; i++) {
+           System.out.println(i);
+        }
     }
 }
